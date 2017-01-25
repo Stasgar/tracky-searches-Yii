2a31702372532d40c914c86e592ad8bd55e672da
Stas Bogatov <stas_garcia@mail.ru>
2017-01-26 00:30:38 +0300

    added reCaptcha password recovery validation

--- config/web.php
+++ config/web.php
@@ -21,6 +21,12 @@ $config = [
         ],
     ],
     'components' => [
+        'reCaptcha' => [
+            'name' => 'reCaptcha',
+            'class' => '\himiklab\yii2\recaptcha\ReCaptcha',
+            'siteKey' => 'your-sitekey',
+            'secret' => 'your-secret',
+        ],
         'view' => [
             'class' => 'yii\web\View',
             'renderers' => [
