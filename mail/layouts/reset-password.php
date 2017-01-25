41c12699c23d8f96c7876b4720fa3a35e2666a44
Stas Bogatov <stas_garcia@mail.ru>
2017-01-25 17:25:15 +0300

    Recover password email template added

---
+++ mail/layouts/reset-password.php
@@ -0,0 +1,30 @@
+<div style="margin:5px;">
+
+    <div style="margin:10px;box-shadow:0 0 10px black; font-size:150%;">
+
+        <div style="text-align:center;">
+
+            <div class="header" style="text-align:left; background-color:#4E5D6C; padding:20px">
+                
+                <span>
+                <img style="vertical-align:middle" src="http://tracky-searches.ru/images/logo_img.png" width="28px" height="28px" alt="">
+                    <a href="http://tracky-searches.ru" style="color:white;text-decoration: none;">
+                    <span style="color:#ff4d4d;">Tracky</span>
+                    searches
+                    </a>
+                </span>
+                
+            </div>
+
+            <div class="content" style="padding:40px; background-color:#2B3E50; color:white">
+                Для изменения пароля перейдите по <a style="color:#DF692A" href="<?= $resetLink ?>">ссылке</a>
+            </div>
+
+            <div class="footer" style="padding:18px; font-size:80%;background-color:#4E5D6C; color:white">
+                <a style="color:#DF692A" href="http://tracky-searches.ru">Tracky-searches.ru</a> - сервис для отслеживания посылок
+            </div>
+
+        </div>
+
+    </div> 
+</div>
