d5a0ff006f356e1edbb415761f29390ae5313c69
Stas Bogatov <stas_garcia@mail.ru>
2017-01-25 17:32:28 +0300

    PasswordManagerController added

---
+++ modules/auth/controllers/PasswordManagementController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace app\modules\auth\controllers;
+use yii\web\Controller;
+use yii\helpers\Url;
+use app\models\User;
+use Yii;
+
+use app\modules\auth\models\PasswordResetRequestForm;
+use app\modules\auth\models\PasswordChangeForm;
+use app\modules\auth\models\ResetPasswordToken;
+
+class PasswordManagementController extends Controller
+{
+    public function actionChangePassword($token = false)
+    {
+        if($token) //если token получен, то производим восстановление пароля
+        {
+            $modelPasswordChange = new PasswordChangeForm();
+
+            $resetPasswordModel = ResetPasswordToken::findOne(['token' => $token]);
+            if($resetPasswordModel)
+            {
+                if($resetPasswordModel->expires > Date('Y-m-d h:m:s'))
+                {
+                    if(Yii::$app->request->post('PasswordChangeForm'))
+                    {//echo'hui1';die;
+                        $modelPasswordChange->attributes = Yii::$app->request->post('PasswordChangeForm');
+
+                        if($modelPasswordChange->validate())
+                        {
+                            //echo'hui2';die;
+                            $modelPasswordChange->changeUserPassword($resetPasswordModel);
+                            return $this->render('ChangeSuccess');
+                        }
+                        
+                    }
+                    //echo'hui01';die;
+                    return $this->render('PasswordChange', ['model' => $modelPasswordChange]);
+                }
+                else
+                {
+                    echo 'token is not valid anymore, it stays active only for 30 mins';
+                    return $this->goHome();
+                }
+            }
+
+                echo 'token is not valid';
+                return $this->goHome();
+
+        }
+        else //если токена нет, то производим изменение п
+        {
+            echo 'no reset token';die;
+        }
+        
+    }
+
+    public function actionResetPasswordRequest()
+    {
+        $model = new PasswordResetRequestForm();
+
+        if(Yii::$app->request->Post('PasswordResetRequestForm'))
+        {
+            $model->attributes = Yii::$app->request->Post('PasswordResetRequestForm');
+
+            if($model->validate())
+            {
+                $model->sendResetLink();
+                return $this->render('ResetSuccess');
+            }
+        }
+
+        return $this->render('PasswordResetRequestForm', ['model'=>$model]);
+    }
+}
