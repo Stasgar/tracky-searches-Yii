53b10b30054f22cb92d0d104e1a7603a0ac034f7
Stas Bogatov <stas_garcia@mail.ru>
2017-03-05 02:14:35 +0300

    reCaptcha config typo fix

--- config/reCaptcha_data.php
+++ config/reCaptcha_data.php
@@ -3,5 +3,5 @@ return [
     'name' => 'reCaptcha',
     'class' => '\himiklab\yii2\recaptcha\ReCaptcha',
     'siteKey' => 'your-site-key',
-    'secret' => 'your-secred-key',
+    'secret' => 'your-secret-key',
 ];
