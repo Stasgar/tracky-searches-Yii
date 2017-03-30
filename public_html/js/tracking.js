c29ec757f6344617966eedc81ca6222d10363cd4
Stas Bogatov <stas_garcia@mail.ru>
2017-03-30 17:54:44 +0300

    Tracking.js tabs fix

--- public_html/js/tracking.js
+++ public_html/js/tracking.js
@@ -1,16 +1,16 @@
 //обработка ajax запроса поиска трек-номера
 $('#search').on('pjax:send', function() {
-  $('.spinner').show(500);
-  $('#search-results').hide(500);
+    $('.spinner').show(500);
+    $('#search-results').hide(500);
 })
 $('#search').on('pjax:complete', function() {
-  $('#search-results').show(500);
-  $('.spinner').hide(500);
-  window.history.replaceState(null, null, "?track="+$('#track-tracknumber').val());
+    $('#search-results').show(500);
+    $('.spinner').hide(500);
+    window.history.replaceState(null, null, "?track="+$('#track-tracknumber').val());
 })
 $('#search').on('pjax:error', function() {
-  $('#canceled-error').show();
-  $('#search-results').hide();
+    $('#canceled-error').show();
+    $('#search-results').hide();
 })
 
 //Обработчик кнопки отследить в разделе "сохраненные трек-номера"
